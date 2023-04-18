ad7e0042a94f3fc82c7f022113dcafd1dd893208
Pedro Moreira <100865375+pedrohenriquemoreira@users.noreply.github.com>
2023-04-18 16:03:50 -0300

    feat: adicionanto stitches para estilização das páginas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(.8)'
+  }
+})
+
 export default function Home() {
   return (
-   <div>
-    <h1>Hello World</h1>
-   </div>
+    <div>
+      <Button><span>teste</span> Enviar</Button>
+    </div>
   )
 }
